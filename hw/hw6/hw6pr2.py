cd0bac223490983ad02649c938b56c46ea97119f
Sam Mikes <smikes@apple.com>
2020-06-30 17:02:59 -0600

    convert to spc

--- hw/hw6/hw6pr2.py
+++ hw/hw6/hw6pr2.py
@@ -12,7 +12,7 @@ because of errors.
 
 Note:
 1. When filling out the functions below, note that
-	1) Let k be the rank for approximation
+        1) Let k be the rank for approximation
 
 2. Please read the instructions and hints carefully, and use the name of the
 variables we provided, otherwise, the function may not work.
@@ -28,87 +28,87 @@ import urllib
 
 if __name__ == '__main__':
 
-	# =============STEP 0: LOADING DATA=================
-	# NOTE: Be sure to install Pillow with "pip3 install Pillow"
-	print('==> Loading image data...')
-	img = ndimage.imread(urllib.request.urlopen('http://i.imgur.com/X017qGH.jpg'), flatten=True)
+        # =============STEP 0: LOADING DATA=================
+        # NOTE: Be sure to install Pillow with "pip3 install Pillow"
+        print('==> Loading image data...')
+        img = imageio.imread('X017qGH.jpg', as_gray=True)
 
-	# TODO: Shuffle the image
+        # TODO: Shuffle the image
 
-	# HINT:
-		# 1) Use np.random.shuffle(img) to shuffle an image img
-		# 2) np.random.shuffle() only shuffle along the major axis (row).
-		# 	Be sure to flatten the image with img.flatten() before doing the shuffling
+        # HINT:
+                # 1) Use np.random.shuffle(img) to shuffle an image img
+                # 2) np.random.shuffle() only shuffle along the major axis (row).
+                #       Be sure to flatten the image with img.flatten() before doing the shuffling
 
-	"*** YOUR CODE HERE ***"
-
-
-	"*** END YOUR CODE HERE ***"
-	# reshape the shuffled image
-	shuffle_img = shuffle_img.reshape(img.shape)
-
-	# =============STEP 1: RUNNING SVD ON IMAGES=================
-	print('==> Running SVD on images...')
-
-	# TODO: SVD on img and shuffle_img
-
-	# HINT:
-	# 		1) Use np.linalg.svd() to perform singular value decomposition
-	# 		2) For the naming of variables, decompose img into U, S, V
-	# 		3) Decompose shuffle_img into U_s, S_s, V_s
-
-	"*** YOUR CODE HERE ***"
+        "*** YOUR CODE HERE ***"
 
 
-	"*** END YOUR CODE HERE ***"
+        "*** END YOUR CODE HERE ***"
+        # reshape the shuffled image
+        shuffle_img = shuffle_img.reshape(img.shape)
 
-	# =============STEP 2: SINGULAR VALUE DROPOFF=================
-	print('==> Singular value dropoff plot...')
-	k = 100
-	plt.style.use('ggplot')
-	# TODO: Generate singular value dropoff plot
+        # =============STEP 1: RUNNING SVD ON IMAGES=================
+        print('==> Running SVD on images...')
 
-	# NOTE:
-	# 		1) Make sure to generate lines with different colors or markers
+        # TODO: SVD on img and shuffle_img
 
-	"*** YOUR CODE HERE ***"
+        # HINT:
+        #               1) Use np.linalg.svd() to perform singular value decomposition
+        #               2) For the naming of variables, decompose img into U, S, V
+        #               3) Decompose shuffle_img into U_s, S_s, V_s
 
-
-	"*** END YOUR CODE HERE ***"
-
-	plt.legend((orig_S_plot, shuf_S_plot), \
-		('original', 'shuffled'), loc = 'best')
-	plt.title('Singular Value Dropoff for Clown Image')
-	plt.ylabel('singular values')
-	plt.savefig('dropoff.png', format='png')
-	plt.close()
-
-	# =============STEP 3: RECONSTRUCTION=================
-	print('==> Reconstruction with different ranks...')
-	rank_list = [2, 10, 20]
-	plt.subplot(2, 2, 1)
-	plt.imshow(img, cmap='Greys_r')
-	plt.axis('off')
-	plt.title('Original Image')
-
-	# TODO: Generate reconstruction images for each of the rank values
-
-	# HINT:
-	# 		1) Use plt.imshow() to display images
-	# 		2) Set cmap='Greys_r' in imshow() to display grey scale images
-
-	for index in range(len(rank_list)):
-		k = rank_list[index]
-		plt.subplot(2, 2, 2 + index)
-
-		"*** YOUR CODE HERE ***"
+        "*** YOUR CODE HERE ***"
 
 
-		"*** END YOUR CODE HERE ***"
+        "*** END YOUR CODE HERE ***"
 
-		plt.title('Rank {} Approximation'.format(k))
-		plt.axis('off')
+        # =============STEP 2: SINGULAR VALUE DROPOFF=================
+        print('==> Singular value dropoff plot...')
+        k = 100
+        plt.style.use('ggplot')
+        # TODO: Generate singular value dropoff plot
 
-	plt.tight_layout()
-	plt.savefig('reconstruction.png', format='png')
-	plt.close()
+        # NOTE:
+        #               1) Make sure to generate lines with different colors or markers
+
+        "*** YOUR CODE HERE ***"
+
+
+        "*** END YOUR CODE HERE ***"
+
+        plt.legend((orig_S_plot, shuf_S_plot), \
+                ('original', 'shuffled'), loc = 'best')
+        plt.title('Singular Value Dropoff for Clown Image')
+        plt.ylabel('singular values')
+        plt.savefig('dropoff.png', format='png')
+        plt.close()
+
+        # =============STEP 3: RECONSTRUCTION=================
+        print('==> Reconstruction with different ranks...')
+        rank_list = [2, 10, 20]
+        plt.subplot(2, 2, 1)
+        plt.imshow(img, cmap='Greys_r')
+        plt.axis('off')
+        plt.title('Original Image')
+
+        # TODO: Generate reconstruction images for each of the rank values
+
+        # HINT:
+        #               1) Use plt.imshow() to display images
+        #               2) Set cmap='Greys_r' in imshow() to display grey scale images
+
+        for index in range(len(rank_list)):
+                k = rank_list[index]
+                plt.subplot(2, 2, 2 + index)
+
+                "*** YOUR CODE HERE ***"
+
+
+                "*** END YOUR CODE HERE ***"
+
+                plt.title('Rank {} Approximation'.format(k))
+                plt.axis('off')
+
+        plt.tight_layout()
+        plt.savefig('reconstruction.png', format='png')
+        plt.close()
